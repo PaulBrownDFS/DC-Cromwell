0a4b358e4c748898a53dfef1c4494f57e6b49231
Paul Brown <paul.brown@dfs.co.uk>
2021-04-09 16:17:53 +0100

    chnaged to vis Api Route

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "da28736fc61fe69762f5";
+/******/ 	var hotCurrentHash = "92c7cab910689b7f20f8";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
