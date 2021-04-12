170116efb968652f8edefab2c6e1810d4ee7e936
Paul Brown <paul.brown@dfs.co.uk>
2021-04-12 16:57:56 +0100

    fetch url fix

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "98a83df24e2f6de1d62c";
+/******/ 	var hotCurrentHash = "51b3df8b9adc52df1b65";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
