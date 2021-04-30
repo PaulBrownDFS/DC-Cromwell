5743473961fe68a05d3ecd4ac5857f1cd249b9ca
Paul Brown <paul.brown@dfs.co.uk>
2021-04-30 10:16:24 +0100

    updated breakpoint fixed widths banner test

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "acab300f0dadc19fd54e";
+/******/ 	var hotCurrentHash = "6d48bb1f13bf5c079f6f";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
