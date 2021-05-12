69b194a5442c616b7b2ecef824b9f4217240e336
Paul Brown <paul.brown@dfs.co.uk>
2021-05-12 09:11:17 +0100

    refactor build with name conventions

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Index() {
     <Container maxWidth="sm">
       <Box my={4}>
         <Typography variant="h4" component="h1" gutterBottom>
-          Next.js Dynamic Content Tools v1.0
+          Next.js Dynamic Content Tools v1.01
         </Typography>
         <Link href="/bannerTest">Banner Test</Link>-
         <Link href="/typog">Typography Demo</Link> -
