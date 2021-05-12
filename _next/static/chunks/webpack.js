6a5d02646b7a024dd9099ca277e19d6024a2984d
Paul Brown <paul.brown@dfs.co.uk>
2021-05-12 10:44:09 +0100

    fixed spacing on card single component

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f9299bbf07eade368579";
+/******/ 	var hotCurrentHash = "8fcd087026b6d63fceca";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
