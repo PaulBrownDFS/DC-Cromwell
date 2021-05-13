79734947cf0facded536d2d1b93fb1678eeba9a5
Paul Brown <paul.brown@dfs.co.uk>
2021-05-13 10:13:32 +0100

    content002 update

--- out/_next/static/chunks/webpack.js
+++ out/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f1131750bd925aff59f3";
+/******/ 	var hotCurrentHash = "063e6d90bc36f7a2cef6";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- src/components/content-002/card.js
+++ src/components/content-002/card.js
@@ -10,6 +10,12 @@ import { makeStyles, useTheme } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
   c002_card: {
+    '& h5': {
+      marginTop: '8px',
+    },
+    '& a': {
+      textDecoration: 'none',
+    },
     '& img.cardImage_c002': {
       // fallback css image size for browsers that do not support <picture>
       [theme.breakpoints.up('sm')]: {
@@ -35,79 +41,83 @@ const Card = ({ card }) => {
   const mdDown = useMediaQuery(theme.breakpoints.down('md'));
   return (
     <Grid item sm={6} md={3} className={classes.c002_card}>
-      <Box position="relative">
-        <picture>
-          {/* WEBP */}
-          <source
-            type="image/webp"
-            media="(min-width: 1920px)"
-            srcSet={`https://images.dfs.co.uk/i/dfs/${card.cardImage.image.name}${seoText}?w=322&fmt.jpeg.interlaced=true&fmt=webp${tt}`}
-          />
-          <source
-            type="image/webp"
-            media="(min-width: 1400px)"
-            srcSet={`https://images.dfs.co.uk/i/dfs/${card.cardImage.image.name}${seoText}?w=282&fmt.jpeg.interlaced=true&fmt=webp${tt}`}
-          />
-          <source
-            type="image/webp"
-            media="(min-width: 768px)"
-            srcSet={`https://images.dfs.co.uk/i/dfs/${card.cardImage.image.name}${seoText}?w=174&fmt.jpeg.interlaced=true&fmt=webp${tt}`}
-          />
-          <source
-            type="image/webp"
-            media="(max-width: 767px)"
-            srcSet={`https://images.dfs.co.uk/i/dfs/${card.cardImage.image.name}${seoText}?w=162&fmt.jpeg.interlaced=true&fmt=webp${tt}`}
-          />
+      {card.cardBtnLink && (
+        <a href={card.cardBtnLink}>
+          <Box position="relative">
+            <picture>
+              {/* WEBP */}
+              <source
+                type="image/webp"
+                media="(min-width: 1920px)"
+                srcSet={`https://images.dfs.co.uk/i/dfs/${card.cardImage.image.name}${seoText}?w=322&fmt.jpeg.interlaced=true&fmt=webp${tt}`}
+              />
+              <source
+                type="image/webp"
+                media="(min-width: 1400px)"
+                srcSet={`https://images.dfs.co.uk/i/dfs/${card.cardImage.image.name}${seoText}?w=282&fmt.jpeg.interlaced=true&fmt=webp${tt}`}
+              />
+              <source
+                type="image/webp"
+                media="(min-width: 768px)"
+                srcSet={`https://images.dfs.co.uk/i/dfs/${card.cardImage.image.name}${seoText}?w=174&fmt.jpeg.interlaced=true&fmt=webp${tt}`}
+              />
+              <source
+                type="image/webp"
+                media="(max-width: 767px)"
+                srcSet={`https://images.dfs.co.uk/i/dfs/${card.cardImage.image.name}${seoText}?w=162&fmt.jpeg.interlaced=true&fmt=webp${tt}`}
+              />
 
-          {/* JPG */}
-          <source
-            media="(min-width: 1920px)"
-            srcSet={`https://images.dfs.co.uk/i/dfs/${card.cardImage.image.name}${seoText}?w=322${tt}`}
-          />
-          <source
-            media="(min-width: 1400px)"
-            srcSet={`https://images.dfs.co.uk/i/dfs/${card.cardImage.image.name}${seoText}?w=282${tt}`}
-          />
-          <source
-            media="(min-width: 768px)"
-            srcSet={`https://images.dfs.co.uk/i/dfs/${card.cardImage.image.name}${seoText}?w=174${tt}`}
-          />
-          <source
-            media="(max-width: 767px)"
-            srcSet={`https://images.dfs.co.uk/i/dfs/${card.cardImage.image.name}${seoText}?w=162${tt}`}
-          />
-
-          <img
-            className="cardImage_c002"
-            src={`https://images.dfs.co.uk/i/dfs/${card.cardImage.image.name}${seoText}?w=322${tt}`}
-            alt={card.cardImage.imageAltText || 'DFS content card image'}
-          />
-        </picture>
-        {/* Roundel */}
-        {card.roundel && ( // conditional rendering based on data returned in request
-          <Box className="RoundelImg">
-            <img
-              className={`cardRoundel rdl_${card.roundel.roundelPosition}`}
-              src={`https://dfs.a.bigcontent.io/v1/static/${card.roundel.roundel.name}`}
-              width={mdDown ? `${card.roundel.roundelSize}%` : 'auto'}
-            />
+              {/* JPG */}
+              <source
+                media="(min-width: 1920px)"
+                srcSet={`https://images.dfs.co.uk/i/dfs/${card.cardImage.image.name}${seoText}?w=322${tt}`}
+              />
+              <source
+                media="(min-width: 1400px)"
+                srcSet={`https://images.dfs.co.uk/i/dfs/${card.cardImage.image.name}${seoText}?w=282${tt}`}
+              />
+              <source
+                media="(min-width: 768px)"
+                srcSet={`https://images.dfs.co.uk/i/dfs/${card.cardImage.image.name}${seoText}?w=174${tt}`}
+              />
+              <source
+                media="(max-width: 767px)"
+                srcSet={`https://images.dfs.co.uk/i/dfs/${card.cardImage.image.name}${seoText}?w=162${tt}`}
+              />
+              <img
+                className="cardImage_c002"
+                src={`https://images.dfs.co.uk/i/dfs/${card.cardImage.image.name}${seoText}?w=322${tt}`}
+                alt={card.cardImage.imageAltText || 'DFS content card image'}
+              />
+            </picture>
+            {/* Roundel */}
+            {card.roundel && ( // conditional rendering based on data returned in request
+              <Box className="RoundelImg">
+                <img
+                  className={`cardRoundel rdl_${card.roundel.roundelPosition}`}
+                  src={`https://dfs.a.bigcontent.io/v1/static/${card.roundel.roundel.name}`}
+                  width={mdDown ? `${card.roundel.roundelSize}%` : 'auto'}
+                />
+              </Box>
+            )}
           </Box>
-        )}
-      </Box>
-      {card.cardHeading ? (
-        <h3>{card.cardHeading}</h3>
-      ) : (
-        <Box m={2}>{/*spacer*/}</Box>
-      )}
 
-      {card.buttonType && (
-        <Button
-          variant={card.buttonType}
-          color={card.buttonColour}
-          href={card.cardBtnLink}
-        >
-          {card.cardBtnText}
-        </Button>
+          {card.cardHeading ? (
+            <h5>{card.cardHeading}</h5>
+          ) : (
+            <Box m={2}>{/*spacer*/}</Box>
+          )}
+
+          {card.buttonType && (
+            <Button
+              variant={card.buttonType}
+              color={card.buttonColour}
+              href={card.cardBtnLink}
+            >
+              {card.cardBtnText}
+            </Button>
+          )}
+        </a>
       )}
     </Grid>
   );
